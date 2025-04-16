7a62442fb3a22e9c33136628885dd681af514887
Para011 <maryamparanai@gmail.com>
2025-04-16 13:47:31 +0300

    Delete week1_assignment1.py

--- week1_assignment1.py
+++
@@ -1,6 +0,0 @@
-print("Hello, Welcome!")
-
-name = input('What is your name?: ')
-fav_colour = input("What is your favourite colour? ")
-
-print("Hello " + name + "! Your favourite colour, " + fav_colour + ", is awesome!")
