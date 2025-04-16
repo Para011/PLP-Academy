7339069968b8b139a93436ee1748b48dc6a57968
Para011 <maryamparanai@gmail.com>
2025-04-16 13:38:58 +0300

    Add files via upload

---
+++ week1_assignment1.py
@@ -0,0 +1,6 @@
+print("Hello, Welcome!")
+
+name = input('What is your name?: ')
+fav_colour = input("What is your favourite colour? ")
+
+print("Hello " + name + "! Your favourite colour, " + fav_colour + ", is awesome!")
